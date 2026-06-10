5d08d1958474c440872b5f54ce536b5e6390d7ff
effloresco <1@mail.com>
2026-06-10 21:10:13 +0300

    feat: add empty board

--- server.js
+++ server.js
@@ -6,4 +6,14 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
+function createEmptyBoard() {
+    let board = [];
+    for (let y = 1; y <= BOARD_SIZE; y++) {
+        for (let x = 1; x <= BOARD_SIZE; x++) {
+            board.push({ X: x, Y: y, Status: 0 });
+        }
+    }
+    return board;
+}
+
 server.listen(3000, () => console.log('Server is online on port 3000'));
